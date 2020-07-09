515e9ab432c0f8c9655cebbcd312939476b91948
heislandmine <heislandmine@protonmail.com>
2020-07-09 20:01:49 +0900

    メソッドの分割

--- downloader.py
+++ downloader.py
@@ -8,41 +8,70 @@ class XtubeDownloader:
         self.root_dir = root_dir
 
     def download_video(self, url, driver):
-        driver.get(url)
-        html = BeautifulSoup(driver.page_source, 'html.parser')
-        try:
-            # 投稿者の名前を取得
-            user_name = self._get_user_name(html)
-        except AttributeError: # 広告が入り込んだ場合の対応
-            with open("skip_urls.txt", "a") as f:
-                print("AttributeError:{}".format(url), file=f)
-
-            print("AttributeError:{}".format(url))
-            return 
+        html = self._get_html(driver, url)
+        # 投稿者の名前を取得
+        user_name = self._get_user_name(html)
         # 動画のダウンロード
-        source = html.find("source", type="video/mp4")
-        if source: # 上にtry文を追加したのでいらないかも
-            print("ダウンロード開始:{}".format(url))
-            video_url = source.get("src")
-            res = get(video_url)
-            _, tmp = tempfile.mkstemp(suffix=".mp4")
-            save_dir = self.root_dir + user_name
-            if not os.path.exists(save_dir):
-                os.mkdir(save_dir)
-            file_name = save_dir + "/" + os.path.basename(tmp)
-            with open(file_name, "wb") as fo:
-                fo.write(res.content)
-            with open("url_list.txt", "a") as f: # このクラスで処理するのが適正か？？？
-                print(url, file=f)
-            print("ダウンロード完了:{}".format(url))
-        else:
-            with open("skip_urls.txt", "a") as f:
-                print("source取得失敗:{}".format(url), file=f)
-
-            print("source取得失敗:{}".format(url))
+        video_url = self._get_video_url(html)
+        self._print_start_download(url)
+        res = get(video_url)
+        save_path = self.get_save_path(user_name)
+        self._save_video(save_path, res.content)
+        self._print_end_download(url)
 
     def _get_user_name(self, html):
         user_info = html.find("a", class_="profileUsername nickname js-pop")
         user_name = user_info.text.strip()
 
         return user_name
+
+    def _get_html(self, driver, url):
+        driver.get(url)
+        html = BeautifulSoup(driver.page_source, 'html.parser')
+
+        return html
+
+    def _get_video_url(self, html):
+        source = html.find("source", type="video/mp4")
+        video_url = source.get("src")
+        return video_url
+
+    def _get_file_name(self):
+        _, tmp = tempfile.mkstemp(suffix=".mp4")
+        file_name = os.path.basename(tmp)
+        return file_name
+
+    def _get_save_dir(self, user_name):
+        file_name = self._get_file_name()
+        save_dir = self.root_dir + user_name
+
+        return save_dir
+
+    def _get_save_path(self, save_dir, file_name):
+        save_path = save_dir + "/" + file_name
+
+        return save_path
+
+    def get_save_path(self, user_name):
+        file_name = self._get_file_name()
+        save_dir = self._get_save_dir(user_name)
+        save_path = self._get_save_path(save_dir, file_name)
+        
+        if not os.path.exists(save_dir):
+            os.mkdir(save_dir)
+
+        return save_path
+
+    def _save_video(self, save_path, video):
+        with open(save_path, "wb") as fo:
+            fo.write(video)
+
+    def _save_visited_list(self):
+        with open("url_list.txt", "a") as f: # このクラスで処理するのが適正か？？？
+            print(url, file=f)
+
+    def _print_start_download(self, url):
+            print("ダウンロード開始:{}".format(url))
+
+    def _print_end_download(self, url):
+            print("ダウンロード完了:{}".format(url))
