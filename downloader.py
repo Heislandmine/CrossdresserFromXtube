59324dc4ce90d19d6f9742cde684d7ca991388f0
heislandmine <heislandmine@protonmail.com>
2020-07-10 18:59:45 +0900

    リファクタリング

--- downloader.py
+++ downloader.py
@@ -9,20 +9,32 @@ class FailedGetVideoPageError(Exception):
     pass
 
 
-class XtubeDownloader:
+class Downloader:
+    def _save_content(self, content, save_path):
+        with open(save_path, "wb") as fo:
+            fo.write(content)
+
+
+class XtubeDownloader(Downloader):
     def __init__(self, root_dir):
         self.root_dir = root_dir
 
     def download_video(self, url, driver):
+        # htmlの取得
         html = self._get_html(driver, url)
         # 投稿者の名前を取得
         user_name = self._get_user_name(html)
-        # 動画のダウンロード
+        # 動画のURLを抽出
         video_url = self._get_video_url(html)
+        # 開始メッセージ
         self._print_start_download(url)
+        # コンテンツを取得
         res = get(video_url)
+        # 保存先を取得
         save_path = self.get_save_path(user_name)
-        self._save_video(save_path, res.content)
+        # 保存
+        self._save_content(res.content, save_path)
+        # 終了メッセージ
         self._print_end_download(url)
 
     def _get_user_name(self, html):
@@ -31,16 +43,16 @@ class XtubeDownloader:
 
         return user_name
 
-    def _get_html(driver, url):
+    def _get_html(self, driver, url):
         driver.get(url)
-        html = BeautifulSoup(driver.page_source, 'html.parser')
+        html = BeautifulSoup(driver.page_source, "html.parser")
 
         if not html:
             raise FailedGetVideoPageError("failed to get video page")
 
         return html
 
-    def _get_video_url(html):
+    def _get_video_url(self, html):
         source = html.find("source", type="video/mp4")
         video_url = source.get("src")
         return video_url
@@ -51,7 +63,6 @@ class XtubeDownloader:
         return file_name
 
     def _get_save_dir(self, user_name):
-        file_name = self._get_file_name()
         save_dir = self.root_dir + user_name
 
         return save_dir
@@ -65,22 +76,19 @@ class XtubeDownloader:
         file_name = self._get_file_name()
         save_dir = self._get_save_dir(user_name)
         save_path = self._get_save_path(save_dir, file_name)
-        
+
         if not os.path.exists(save_dir):
             os.mkdir(save_dir)
 
         return save_path
 
-    def _save_video(self, save_path, video):
-        with open(save_path, "wb") as fo:
-            fo.write(video)
-
     def _save_visited_list(self):
-        with open("url_list.txt", "a") as f: # このクラスで処理するのが適正か？？？
+        with open("url_list.txt", "a") as f:  # このクラスで処理するのが適正か？？？
             print(url, file=f)
 
     def _print_start_download(self, url):
-            print("ダウンロード開始:{}".format(url))
+        print("ダウンロード開始:{}".format(url))
 
     def _print_end_download(self, url):
-            print("ダウンロード完了:{}".format(url))
+        print("ダウンロード完了:{}".format(url))
+
